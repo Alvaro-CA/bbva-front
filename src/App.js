2d3222d59cbff9f3bfd18b6acc26d8d2ef80f728
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-08-27 15:52:52 -0500

    incluide nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Routes, Route, } from 'react-router-dom';
 import Landing from './features/landing/landing';
+import AforoOficina from './features/aforoOficina/AforoOficina';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Landing />} />
+      <Route path='/aforo' element={<AforoOficina />} />
       <Route path='/*' element={<>Página no encontrada</>} />
     </Routes>
   );
