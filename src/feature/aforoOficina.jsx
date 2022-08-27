f0cdc15e679eab57e31986673a09388fdae5e61e
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-08-27 13:58:29 -0500

    move feature

---
+++ src/feature/aforoOficina.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AforoOficina(){
+
+    const myColumn= styled('div')`
+    background: black;
+    color: white;
+    display: flex;
+    flex: row;
+  `
+
+    return(
+     <>
+        <myColumn>HOLA </myColumn>
+        <myColumn>MUNDO</myColumn>
+    
+
+
+     </>
+    )
+}
