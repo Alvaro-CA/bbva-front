a320e36d0aed59f6e4ffacf621f1da27fc9ce19b
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-08-28 12:08:43 -0500

    add banner

---
+++ src/features/landing/banner/banner1.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Banner1 (){
+
+    const ContainerBannerLeft = styled.div`
+    background-color: rgb(3 105 161);
+    height: 550px;
+    z-index:1;
+    clip-path: polygon(60% 0, 45% 100%, 0 100%, 0 0%);
+    `
+
+
+    const ContainerBannerText = styled.div`
+    
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    `
+
+    const TextBanner1= styled.h3`
+
+    color: rgb(20 184 166);
+    font-weight: 500;
+    font-size: 16px;
+    text-align: left;
+    margin-top: 0px;
+    margin-bottom: 0px;
+
+   `
+
+   const TextBanner2= styled.h3`
+
+    color: rgb(249 250 251);
+    font-weight: 700;
+    font-size: 45px;
+    text-align: left;
+    margin-top: 0px;
+    margin-bottom: 0px;
+
+   `
+
+   const TextBanner3= styled.h3`
+
+    color: rgb(245 158 11);
+    font-weight: 700;
+    font-size: 45px;
+    text-align: left;
+    margin-top: 0px;
+    margin-bottom: 0px;
+
+   `
+
+   const TextBanner4= styled.h3`
+
+    color: rgb(249 250 251);
+    font-weight: 600;
+    text-align: left;
+    font-size: 15px;
+    margin-top: 20px;
+    margin-bottom: 0px;
+
+   `
+    
+    const ContainerBannerRight = styled.div`
+      background-image: url(../../../assets/images/img2.jpg);
+      background-size:200px; 
+      background-repeat:no-repeat; 
+      background-position:right bottom;
+      width:30px;
+      `
+
+   return (
+    <>
+    <ContainerBannerLeft>
+        <ContainerBannerText>
+            <TextBanner1>Seguro de Bienes</TextBanner1>
+            <TextBanner2>Descubre nuestro nuevo</TextBanner2>
+            <TextBanner3>Seguro Hogar Total</TextBanner3>
+            <TextBanner4>Hecho 100% a tu medida para que protejas lo que tanto esfuerzo te costó tener.</TextBanner4>
+        </ContainerBannerText>
+    </ContainerBannerLeft>
+    <ContainerBannerRight></ContainerBannerRight>
+    </>
+   )
+}
+
+export default Banner1
--- src/features/landing/landing.jsx
+++ src/features/landing/landing.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Page from '../../components/page/page'
 import Products from '../../components/page/components/products/products'
+import Banner1 from './banner/banner1'
 
 const Landing = () => {
   return (
     <Page>
-      <div>Landing</div>
+      <Banner1 />
       <Products />
     </Page>
   )
